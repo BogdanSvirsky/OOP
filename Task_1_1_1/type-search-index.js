2e8090badb5d6f85b35a67c7b4d7a5872b116f31
BogdanSvirsky <BogdanSvirsky@users.noreply.github.com>
2024-09-11 04:03:31 +0000

    Deploying to gh-pages from @ BogdanSvirsky/OOP@37fd0e97634a9fd61f3ec37899dfea2f70b75d55 🚀

--- Task_1_1_1/type-search-index.js
+++ Task_1_1_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.svirsky","l":"Heapsort"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.svirsky","l":"Heap"},{"p":"ru.nsu.svirsky","l":"Heapsort"}]
