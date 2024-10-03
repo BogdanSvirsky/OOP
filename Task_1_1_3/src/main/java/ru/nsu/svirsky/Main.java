5a98e2b77cdbcf92a18afde7652ce9a2c3ab3276
Bogdan Svirsky <svirskybogdan@yandex.ru>
2024-10-04 00:52:48 +0700

    task_1_1_3: refactor

--- Task_1_1_3/src/main/java/ru/nsu/svirsky/Main.java
+++ Task_1_1_3/src/main/java/ru/nsu/svirsky/Main.java
@@ -1,24 +1,19 @@
 package ru.nsu.svirsky;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Expression e = new Add(new Number(3), new Mul(new Number(2),
-                new Variable("x")));
-        Expression ee = Parser.parse("2.25");
-
-        e.print();
-        ee.print();
-
-        Expression de = e.derivative("x");
-        de.print();
-        de.simplify().print();
-        Parser.parse("((0 * x) + (2 * x))").simplify().print();
-
-        e = new Add(new Number(3), new Mul(new Number(2),
-                new Variable("x")));
-
-        double result = e.eval("x = 10; y = 13");
-        System.out.println(result);
-        System.out.println(ee.eval("x=10"));
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter expression (don't forget brackets!): ");
+        Expression expr = Parser.parse(scanner.nextLine());
+        System.out.print("Entet assignment (separate by ';'): ");
+        try {
+            System.out.println("Result: " + String.valueOf(expr.eval(scanner.nextLine())));
+        } catch (ArithmeticException e) {
+            System.out.println("(!) " + e.getMessage());
+        } finally {
+            scanner.close();
+        }
     }
 }
